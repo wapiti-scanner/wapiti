7a2c368c8328107c6b068abdbd64cbe7344a72c8
devloop <nicolas.surribas@gmail.com>
2019-06-21 17:19:04 +0200

    Added some tests for relative links parsing

--- tests/parsers/test_html_parser.py
+++ tests/parsers/test_html_parser.py
@@ -35,3 +35,34 @@ def test_relative_root():
 
         # We will get invalid hostnames with dots. Browsers do that too.
         assert set(page.links) == {url, "http://./", "http://../"}
+
+
+@responses.activate
+def test_relative_links():
+    with open("tests/data/relative_links.html") as fd:
+        url = "http://perdu.com/"
+        responses.add(
+            responses.GET,
+            url,
+            body=fd.read()
+        )
+
+        resp = requests.get(url)
+        page = Page(resp, url)
+
+        # We will get invalid hostnames with dots. Browsers do that too.
+        assert set(page.links) == {
+            url,
+            "http://perdu.com/file.html",
+            "http://perdu.com/resource",
+            "http://perdu.com/folder/",
+            "http://perdu.com/folder/file.html",
+            "http://perdu.com/folder/file2.html",
+            "http://perdu.com/file3.html",
+            "http://perdu.com/?k=v",
+            "http://perdu.com/file3.html?k=v",
+            "http://perdu.com/folder/?k=v",
+            "http://perdu.com/folder?k=v",
+            "http://external.tld/",
+            "http://external.tld/yolo?k=v",
+        }
