6eec875692c74097fd1c518b1611cdf050811261
Nicolas SURRIBAS <nicolas.surribas@cybelangel.com>
2019-07-25 18:07:01 +0200

    Added more tests on scan scopes

--- tests/web/test_scope.py
+++ tests/web/test_scope.py
@@ -1,7 +1,8 @@
 import responses
 import requests
 
-from wapitiCore.net.crawler import Page
+from wapitiCore.net.crawler import Page, Crawler, Scope
+from wapitiCore.net.web import Request
 
 
 @responses.activate
@@ -23,3 +24,62 @@ def test_domain_scope():
     assert not page.is_external_to_domain("https://perdu.com/blog/")
     assert not page.is_external_to_domain("http://perdu.com:80/blog/")
     assert page.is_external_to_domain("http://perdu.com.org/blog/")
+
+
+def test_scopes():
+    links = {
+        "http://perdu.com/",
+        "http://perdu.com/page.html",
+        "http://perdu.com/subdir/subdirpage.html",
+        "http://perdu.com/subdir/subdir2/subdirpage2.html",
+        "http://sub.perdu.com/page.html",
+        "https://perdu.com/secure.html",
+        "http://perdu.com/subdir/page.html?k=v",
+        "http://perdu.com/subdir/page.html",
+        "http://lost.com/lost.html",
+        "http://external.tld/external.html"
+    }
+
+    crawler = Crawler("http://perdu.com/subdir/")
+    crawler.scope = Scope.FOLDER
+    filtered = {link for link in links if crawler.is_in_scope(Request(link))}
+    assert filtered == {
+        "http://perdu.com/subdir/subdirpage.html",
+        "http://perdu.com/subdir/subdir2/subdirpage2.html",
+        "http://perdu.com/subdir/page.html?k=v",
+        "http://perdu.com/subdir/page.html",
+    }
+
+    crawler = Crawler("http://perdu.com/subdir/page.html")
+    crawler.scope = Scope.PAGE
+    filtered = {link for link in links if crawler.is_in_scope(Request(link))}
+    assert filtered == {
+        "http://perdu.com/subdir/page.html?k=v",
+        "http://perdu.com/subdir/page.html"
+    }
+
+    crawler = Crawler("http://perdu.com/subdir/page.html?k=v")
+    crawler.scope = Scope.URL
+    filtered = {link for link in links if crawler.is_in_scope(Request(link))}
+    assert filtered == {
+        "http://perdu.com/subdir/page.html?k=v"
+    }
+
+    crawler = Crawler("http://perdu.com/subdir/page.html?k=v")
+    crawler.scope = Scope.DOMAIN
+    filtered = {link for link in links if crawler.is_in_scope(Request(link))}
+    assert filtered == {
+        "http://perdu.com/",
+        "http://perdu.com/page.html",
+        "http://perdu.com/subdir/subdirpage.html",
+        "http://perdu.com/subdir/subdir2/subdirpage2.html",
+        "http://sub.perdu.com/page.html",
+        "https://perdu.com/secure.html",
+        "http://perdu.com/subdir/page.html?k=v",
+        "http://perdu.com/subdir/page.html"
+    }
+
+    crawler = Crawler("http://perdu.com/subdir/page.html?k=v")
+    crawler.scope = Scope.PUNK
+    filtered = {link for link in links if crawler.is_in_scope(Request(link))}
+    assert filtered == links
