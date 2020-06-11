316d65d9ea8d518d7e6e168f4affc1bc65d0cba4
bretfourbe <gwendal@cyberwatch.fr>
2020-06-11 11:02:19 +0200

    Fix snake_case pylint error from moon.py
    Signed-off-by: bretfourbe <gwendal@cyberwatch.fr>

--- wapitiCore/moon.py
+++ wapitiCore/moon.py
@@ -7,33 +7,33 @@ from datetime import datetime
 # http://smithje.github.io/bash/2013/07/08/moon-phase-prompt.html
 
 def julian(year, month, day) -> float:
-    a = (14 - month) / 12.0
-    y = year + 4800 - a
-    m = (12 * a) - 3 + month
-    return day + (153 * m + 2) / 5.0 + (365 * y) + y / 4.0 - y / 100.0 + y / 400.0 - 32045
+    year_fraction = (14 - month) / 12.0
+    j_year = year + 4800 - year_fraction
+    j_month = (12 * year_fraction) - 3 + month
+    return day + (153 * j_month + 2) / 5.0 + (365 * j_year) + j_year / 4.0 - j_year / 100.0 + j_year / 400.0 - 32045
 
 
 def phase(year=None, month=None, day=None) -> str:
     if year is None and month is None and day is None:
         today = datetime.now()
         year, month, day = today.year, today.month, today.day
-    p = (julian(year, month, day) - julian(2000, 1, 6)) % 29.530588853
+    moon_phase = (julian(year, month, day) - julian(2000, 1, 6)) % 29.530588853
 
-    if p < 1.84566:
+    if moon_phase < 1.84566:
         return "new"
-    elif p < 5.53699:
+    elif moon_phase < 5.53699:
         return "waxing crescent"
-    elif p < 9.22831:
+    elif moon_phase < 9.22831:
         return "first quarter"
-    elif p < 12.91963:
+    elif moon_phase < 12.91963:
         return "waxing gibbous"
-    elif p < 16.61096:
+    elif moon_phase < 16.61096:
         return "full"
-    elif p < 20.30228:
+    elif moon_phase < 20.30228:
         return "waning gibbous"
-    elif p < 23.99361:
+    elif moon_phase < 23.99361:
         return "last quarter"
-    elif p < 27.68493:
+    elif moon_phase < 27.68493:
         return "waning crescent"
     else:
         return "new"
