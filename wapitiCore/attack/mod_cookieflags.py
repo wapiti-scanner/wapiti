73f0d14ca1b9fec69260aaad264eb1784aa60522
devloop <nicolas.surribas@gmail.com>
2020-07-31 23:33:08 +0200

    smode pycharm auto-reformat + use of staticmethod

--- wapitiCore/attack/mod_cookieflags.py
+++ wapitiCore/attack/mod_cookieflags.py
@@ -1,41 +1,41 @@
-import requests
 from wapitiCore.attack.attack import Attack
 from wapitiCore.net.web import Request
 from wapitiCore.language.vulnerability import Additional, _
 
-class mod_cookieflags(Attack):
 
+class mod_cookieflags(Attack):
     name = "cookieflags"
 
-    def check_secure_flag(self, cookie: object):
-
+    @staticmethod
+    def check_secure_flag(cookie: object):
         return cookie.secure
 
-    def check_httponly_flag(self, cookie: object):
-
-        return 'HttpOnly' in cookie._rest
+    @staticmethod
+    def check_httponly_flag(cookie: object):
+        return "HttpOnly" in cookie._rest
 
     def attack(self):
         url = self.persister.get_root_url()
         request = Request(url)
-        cookies=self.crawler.session_cookies
-        for cook in cookies:
-            self.log_blue(_("Checking cookie : {}".format(cook.name)))
-            if not self.check_httponly_flag(cook):
-                self.log_red(Additional.INFO_COOKIE_HTTPONLY.format(cook.name))
+        cookies = self.crawler.session_cookies
+        for cookie in cookies:
+            self.log_blue(_("Checking cookie : {}".format(cookie.name)))
+            if not self.check_httponly_flag(cookie):
+                self.log_red(Additional.INFO_COOKIE_HTTPONLY.format(cookie.name))
                 self.add_addition(
                     category=Additional.COOKIE_HTTPONLY_DISABLED,
                     level=Additional.LOW_LEVEL,
                     request=request,
-                    info=Additional.INFO_COOKIE_HTTPONLY.format(cook.name)
+                    info=Additional.INFO_COOKIE_HTTPONLY.format(cookie.name)
                 )
-            if not self.check_secure_flag(cook):
-                self.log_red(Additional.INFO_COOKIE_SECURE.format(cook.name))
+
+            if not self.check_secure_flag(cookie):
+                self.log_red(Additional.INFO_COOKIE_SECURE.format(cookie.name))
                 self.add_addition(
                     category=Additional.COOKIE_SECURE_DISABLED,
                     level=Additional.LOW_LEVEL,
                     request=request,
-                    info=Additional.INFO_COOKIE_SECURE.format(cook.name)
+                    info=Additional.INFO_COOKIE_SECURE.format(cookie.name)
                 )
 
         yield
