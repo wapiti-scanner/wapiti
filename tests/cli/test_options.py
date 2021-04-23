199df6d52d6339681d2f3ffa947cc0051c34d138
devloop <nicolas.surribas@gmail.com>
2021-04-23 09:20:57 +0200

    Fix more tests

--- tests/cli/test_options.py
+++ tests/cli/test_options.py
@@ -1,52 +1,55 @@
+from asyncio import Event
+
 from wapitiCore.main.wapiti import Wapiti
 from wapitiCore.attack.attack import commons, modules
 
 
 def test_options():
+    stop_event = Event()
     cli = Wapiti("http://perdu.com/", session_dir="/dev/shm")
     cli.set_attack_options({"timeout": 10})
 
     cli.set_modules("-all,xxe")
-    cli._init_attacks()
+    cli._init_attacks(stop_event)
     assert {module.name for module in cli.attacks if module.do_get or module.do_post} == {"xxe"}
 
     cli.set_modules("xxe")
-    cli._init_attacks()
+    cli._init_attacks(stop_event)
     assert {module.name for module in cli.attacks if module.do_get or module.do_post} == {"xxe"}
 
     cli.set_modules("common,xxe")
-    cli._init_attacks()
+    cli._init_attacks(stop_event)
     activated_modules = {module.name for module in cli.attacks if module.do_get or module.do_post}
     assert len(activated_modules) == len(commons) + 1
 
     cli.set_modules("common,-exec")
-    cli._init_attacks()
+    cli._init_attacks(stop_event)
     activated_modules = {module.name for module in cli.attacks if module.do_get or module.do_post}
     assert len(activated_modules) == len(commons) - 1
 
     cli.set_modules("all,-xxe")
-    cli._init_attacks()
+    cli._init_attacks(stop_event)
     activated_modules = {module.name for module in cli.attacks if module.do_get or module.do_post}
     assert len(activated_modules) == len(modules) - 1
 
     cli.set_modules("all,-common")
-    cli._init_attacks()
+    cli._init_attacks(stop_event)
     activated_modules = {module.name for module in cli.attacks if module.do_get or module.do_post}
     assert len(activated_modules) == len(modules) - len(commons)
 
     cli.set_modules("common,-all,xss")
-    cli._init_attacks()
+    cli._init_attacks(stop_event)
     activated_modules = {module.name for module in cli.attacks if module.do_get or module.do_post}
     assert len(activated_modules) == 1
 
     # Empty module list: no modules will be used
     cli.set_modules("")
-    cli._init_attacks()
+    cli._init_attacks(stop_event)
     activated_modules = {module.name for module in cli.attacks if module.do_get or module.do_post}
     assert not activated_modules
 
     # Use default settings: only use "commons" modules
     cli.set_modules(None)
-    cli._init_attacks()
+    cli._init_attacks(stop_event)
     activated_modules = {module.name for module in cli.attacks if module.do_get or module.do_post}
     assert activated_modules == set(commons)
--- tests/parsers/test_html_forms.py
+++ tests/parsers/test_html_forms.py
@@ -67,11 +67,11 @@ def test_forms():
             elif form.file_path == "/upload.php":
                 assert form.is_multipart
                 assert not form.post_params
-                assert form.file_params == [["file", ["pix.gif", "GIF89a", "image/gif"]]]
+                assert form.file_params == [["file", ("pix.gif", "GIF89a", "image/gif")]]
             elif form.file_path == "/alt.php":
                 form_action = True
             elif form.file_path == "/upload_empty_value.php":
-                assert form.file_params == [["file", ["pix.gif", "GIF89a", "image/gif"]]]
+                assert form.file_params == [["file", ("pix.gif", "GIF89a", "image/gif")]]
             elif form.file_path == "/select_no_defaults.php":
                 items = dict(form.post_params)
                 assert items["choices"] == "3rd_choice"
