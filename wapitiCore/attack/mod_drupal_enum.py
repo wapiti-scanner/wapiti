0d32df478788e9f88c36fb87519fa9179cd7aa13
devloop <nicolas.surribas@gmail.com>
2021-07-06 18:09:46 +0200

    Concurrency for mod_drupal_enum

--- wapitiCore/attack/mod_drupal_enum.py
+++ wapitiCore/attack/mod_drupal_enum.py
@@ -1,6 +1,11 @@
+import asyncio
 import json
 import hashlib
+import logging
 from os.path import join as path_join
+from typing import Tuple
+
+from httpx import RequestError
 
 from wapitiCore.net.web import Request
 from wapitiCore.attack.attack import Attack
@@ -19,39 +24,101 @@ class mod_drupal_enum(Attack):
 
     versions = []
 
-    def get_hash(self):
+    def get_hashes(self):
         with open(path_join(self.DATA_DIR, self.PAYLOADS_HASH), errors="ignore") as hashes:
             data = json.load(hashes)
             return data
 
-    async def detect_version(self, url):
-        vers = {}
-        data = self.get_hash()
-        for uri in data:
+    async def get_url_hash(self, root_url: str, path: str) -> Tuple[str, str]:
+        request = Request(f"{root_url}{path}")
+        response = await self.crawler.async_get(request)
+        if response.status != 200:
+            return "", ""
+
+        return hashlib.sha256(response.content.encode()).hexdigest(), path
+
+    async def detect_version(self, root_url):
+        versions = {}
+        detection_db = self.get_hashes()
+        tasks = set()
+
+        for path in detection_db:
+            task = asyncio.create_task(self.get_url_hash(root_url, path))
+            tasks.add(task)
+
+            while True:
+                done_tasks, pending_tasks = await asyncio.wait(
+                    tasks,
+                    timeout=0.01,
+                    return_when=asyncio.FIRST_COMPLETED
+                )
+
+                for task in done_tasks:
+                    try:
+                        content_hash, path = await task
+                    except RequestError:
+                        self.network_errors += 1
+                    else:
+                        if content_hash and content_hash in detection_db[path]:
+                            versions[path] = detection_db[path][content_hash]
+
+                    tasks.remove(task)
+
+                if self._stop_event.is_set():
+                    for task in pending_tasks:
+                        task.cancel()
+                        tasks.remove(task)
+
+                if len(pending_tasks) > self.options["tasks"]:
+                    continue
+
+                break
+
             if self._stop_event.is_set():
                 break
 
-            req = Request('{}{}'.format(url, uri))
-            rep = await self.crawler.async_get(req)
-            if rep.status != 200:
-                continue
+        # We reached the end of your list but we may still have some running tasks
+        while tasks:
+            done_tasks, pending_tasks = await asyncio.wait(
+                tasks,
+                timeout=0.01,
+                return_when=asyncio.FIRST_COMPLETED
+            )
 
-            cont = rep.content.encode()
-            hash_content = hashlib.sha256(cont).hexdigest()
+            for task in done_tasks:
+                try:
+                    content_hash, path = await task
+                except RequestError:
+                    self.network_errors += 1
+                else:
+                    if content_hash and content_hash in detection_db[path]:
+                        versions[path] = detection_db[path][content_hash]
 
-            if uri in data:
-                if hash_content in data[uri]:
-                    vers[uri] = data[uri][hash_content]
-        if vers:
-            self.versions = set.intersection(*[set(versions) for versions in vers.values()])
+                tasks.remove(task)
+
+            if self._stop_event.is_set():
+                for task in pending_tasks:
+                    task.cancel()
+                    tasks.remove(task)
+
+                break
+
+        if versions:
+            self.versions = set.intersection(*[set(versions) for versions in versions.values()])
 
     async def check_drupal(self, url):
         check_list = ['sites/', 'core/misc/drupal.js', 'misc/drupal.js', 'misc/test/error/404/ispresent.html']
         for item in check_list:
-            req = Request('{}{}'.format(url, item))
-            rep = await self.crawler.async_get(req)
-            if rep.status != 404:
-                return True
+            request = Request(f'{url}{item}')
+            try:
+                response = await self.crawler.async_get(request)
+            except RequestError:
+                self.network_errors += 1
+            except Exception as exception:
+                logging.exception(exception)
+            else:
+                if response.status != 404:
+                    return True
         return False
 
     async def must_attack(self, request: Request):
@@ -60,6 +127,7 @@ class mod_drupal_enum(Attack):
 
         if request.method == "POST":
             return False
+
         return request.url == await self.persister.get_root_url()
 
     async def attack(self, request: Request):
