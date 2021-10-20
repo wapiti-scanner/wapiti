bd0df0e1ecad41ef7a74f4df994de5c99efde275
devloop <nicolas.surribas@gmail.com>
2021-10-20 13:37:27 +0200

    Upgrade respx too

--- setup.py
+++ setup.py
@@ -134,7 +134,7 @@ if a script is vulnerable.""",
         ],
     },
     # https://buildmedia.readthedocs.org/media/pdf/pytest/3.6.0/pytest.pdf
-    tests_require=["pytest>=6.2.2", "respx==0.17.0", "pytest-cov>=2.11.1", "pytest-asyncio==0.14.0"],
+    tests_require=["pytest>=6.2.2", "respx==0.18.0", "pytest-cov>=2.11.1", "pytest-asyncio==0.14.0"],
     setup_requires=["pytest-runner"],
     cmdclass={"test": PyTest}
 )
