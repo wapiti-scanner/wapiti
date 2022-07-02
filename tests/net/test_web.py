7b85b8950c0cf0061c6c8c14b0e6a4b79c4b30ef
devloop <nicolas.surribas@gmail.com>
2022-07-02 16:57:07 +0200

    Fix some tests

--- tests/net/test_web.py
+++ tests/net/test_web.py
@@ -4,7 +4,14 @@ from wapitiCore.net.response import Response, detail_response
 
 
 def test_detail_response():
-    response = Response(httpx.Response(200, headers=httpx.Headers([["foo", "bar"]]), content=b"body"))
+    response = Response(
+        httpx.Response(
+            200,
+            headers=httpx.Headers([["foo", "bar"]]),
+            content=b"body"
+        ),
+        url="http://perdu.com/"
+    )
 
     detailed_response = detail_response(response)
 
