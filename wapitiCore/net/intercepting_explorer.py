8cb0ee238a118d710d8369acd8cef9a0762a810d
devloop <nicolas.surribas@gmail.com>
2022-07-29 13:56:09 +0200

    fix style (again)

--- wapitiCore/net/intercepting_explorer.py
+++ wapitiCore/net/intercepting_explorer.py
@@ -17,7 +17,6 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 import asyncio
-from collections import deque
 from typing import Tuple, List, AsyncIterator, Dict, Optional, Deque
 from logging import getLogger, WARNING, ERROR
 from http.cookiejar import CookieJar
