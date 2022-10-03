0519720c85fdf17082dc0a4ca9115add30edd818
devloop <nicolas.surribas@gmail.com>
2022-10-03 12:06:10 +0200

    add regex for parsing window.open urls

--- wapitiCore/parsers/javascript.py
+++ wapitiCore/parsers/javascript.py
@@ -5,9 +5,15 @@ RE_JS_REDIR = re.compile(
     r"\b(window\.|document\.|top\.|self\.)?location(\.href)?\s*=\s*(\"|')([^'\"]+)\3\s*(;|}|$)"
 )
 
+RE_WINDOW_OPEN = re.compile(
+    r"\bwindow\.open\(\s*(\"|')([^'\"]+)\1\s*\)"
+)
+
 
 def extract_js_redirections(text: str) -> List[str]:
     redirections = set()
     for search in re.finditer(RE_JS_REDIR, text):
         redirections.add(search.group(4))
+    for search in re.finditer(RE_WINDOW_OPEN, text):
+        redirections.add(search.group(2))
     return list(redirections)
