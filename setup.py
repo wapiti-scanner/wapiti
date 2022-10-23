9133ae3753d3fea0dc4605c84d12868e95346601
devloop <nicolas.surribas@gmail.com>
2022-10-23 18:05:12 +0200

    upgrade pytest-asyncio

--- setup.py
+++ setup.py
@@ -140,7 +140,7 @@ if a script is vulnerable.""",
         ],
     },
     # https://buildmedia.readthedocs.org/media/pdf/pytest/3.6.0/pytest.pdf
-    tests_require=["pytest>=6.2.2", "respx==0.20.0", "pytest-cov>=2.11.1", "pytest-asyncio==0.14.0"],
+    tests_require=["pytest>=6.2.2", "respx==0.20.0", "pytest-cov>=2.11.1", "pytest-asyncio==0.20.1"],
     setup_requires=["pytest-runner"],
     cmdclass={"test": PyTest}
 )
