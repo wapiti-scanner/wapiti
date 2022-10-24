af82ddfd6aeaf6475691849a37286d163b6939c8
devloop <nicolas.surribas@gmail.com>
2022-10-24 15:44:11 +0200

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from multiprocessing import cpu_count
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 
-VERSION = "3.1.3"
+VERSION = "3.1.4"
 DOC_DIR = "share/doc/wapiti"
 
 
