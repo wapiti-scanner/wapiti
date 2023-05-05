f14dd1b4b9cb8b1ac67b7317db468b43bf8aca73
Ubuntu <romain.levifve@protonmail.com>
2023-05-05 11:51:01 +0000

    static db version for wordpress, container name for wapiti

--- tests/integration/run.sh
+++ tests/integration/run.sh
@@ -1,7 +1,18 @@
 set -e
-#$(cd ../../ && docker build wapiti .);
-#$(cd wp_enum && docker build wordpress .);
+
+# gerer les params
+
+# charger les tests dans wapiti
+
 for file in $(find . -name "docker*")
 do
 	docker compose --verbose -f "$file" up -d
 done
+
+#boucle sur les params
+	# requetes sur les modules
+	# tests des requetes
+	# si test fail, casse la boucle
+# fin boucle
+
+# cleanup process
