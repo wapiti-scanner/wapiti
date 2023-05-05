16f6b9f3ed064ce1d23f5d5d160781c7a55b38e1
RMI78 <romain.escrime@gmail.com>
2023-05-05 08:20:33 +0000

    Adding integration test system

---
+++ tests/integration/run.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+# List of modules to be tested
+TESTS=""
+
+# Normalize spaces for shell substitution
+if [[ ! -z "$TESTS" ]]; then
+    export TESTS="$(echo "$TESTS" | xargs) "
+fi
+
+# exit upon any error
+set -o errexit
+
+# exit upon using undeclared variables
+set -o nounset
+
+# Placing ourselves in the right directory
+cd "$(dirname "$0")"
+
+# Parsing script arguments 
+declare -A args
+for arg in "$@"; do
+    args[$arg]=1;
+done; 
+
+if [[ -v args["--help"] ]]; then
+    # Printing some help
+    printf "%s\n" \
+           "Entrypoint to run integration tests" \
+           "Usage: ./run.sh [options]" \
+           "Options:" \
+           "    --help           Display this message and exit"\
+           "    --docker-clean   Kill containers, remove and prune all docker images, volumes, and system, be carefull when using this option"\
+           "    --verbose-build  Print the build messages before running the tests";
+           exit 0;
+fi
+
+if [[ -v args["--docker-clean"] ]]; then
+    # Cleaning docker
+    echo "Cleaning docker..."
+    docker kill $(docker ps -q) 2> /dev/null || echo "No containers to kill"
+    docker container prune -f 2> /dev/null || echo "No containers to prune"
+    docker volume prune -f 2> /dev/null || echo "No volumes to prune"
+    docker volume rm $(docker volume ls -q) 2> /dev/null || echo "No volume to remove"
+    docker rmi $(docker images -a -q) 2> /dev/null || echo "No images to remove"
+    (docker system prune -f && docker network create test-network) 2> /dev/null || echo "No need to prune the system"
+fi
+
+# Fallback to create the test-network in case it doesn't exist
+docker network inspect test-network > /dev/null || docker network create test-network > /dev/null
+
+echo "Building images..."
+if [[ ! -v args["--verbose-build"] ]];then
+# Quietly build all Dockerfiles
+docker compose -f docker-compose.setup.yml build --quiet
+fi
+
+# Start the tests
+docker compose  --progress quiet -f docker-compose.setup.yml up --abort-on-container-exit
+
+declare -a asserters=()
+# If the TESTS env variable is supplied, we will only check the specified tests
+if [[ ! -z "$TESTS" ]]; then
+    # Assuming all the tests in the TESTS variable are well written and exist
+    mapfile -t asserters < <(echo -e "${TESTS// /\/assertions\/check.sh\\n}" |  head -n -1)
+else
+    # Otherwise, we take all the tests
+    mapfile -t asserters < <(find . -mindepth 2 -type l,f -name check.sh)
+fi
+EXIT_CODE=0
+for path in "${asserters[@]}"; do
+    cd "$(dirname "${path}")" 
+    bash "check.sh" | tee -a ../../.dump_diff_file.txt
+    # Workaround to check if check.sh succeed, may not work on zsh 
+    if [[ "${PIPESTATUS[0]}" -eq 1 ]]; then
+        EXIT_CODE=1
+    fi
+    cd - > /dev/null 
+done
+
+exit $EXIT_CODE
---
+++ tests/integration/wapiti/test.py
@@ -0,0 +1,118 @@
+import json
+import os
+import sys
+import requests
+import re
+from itertools import cycle
+from collections import defaultdict
+from itertools import chain
+from uuid import uuid4
+from time import sleep
+
+from misc_functions import purge_irrelevant_data, filter_data, all_keys_dicts, sort_lists_in_dict
+from templates_and_data import DEFAULT_FILTER_TREE, EXISTING_MODULES, TREE_CHECKER
+
+# parsing and checking the json file containing the modules
+with open('/usr/local/bin/modules.json', 'r') as integration_file:
+    integration_data = json.load(integration_file)
+    wanted_modules = set(chain.from_iterable([test["modules"].split(",")
+                                              for _, test in integration_data.items()]))
+    assert wanted_modules.issubset(EXISTING_MODULES), f"{wanted_modules-EXISTING_MODULES} modules not existing"
+
+# Adding on-the-fly uuid for each target for each test
+# and using quotes for empty modules
+for _, integ_test in integration_data.items():
+    if not len(integ_test["modules"]):
+        integ_test["modules"] = "\"\""
+    for target in integ_test['targets']:
+        target.update({"uid": str(uuid4())})
+
+# Eventually filter arguments if any
+if len(sys.argv) > 1:
+    # first checking unknown tests/typo errors
+    assert set(sys.argv[1:]).issubset(set(key for key, _ in integration_data.items()))
+    # then filtering the wanted integration tests
+    integration_data = {key: test for key, test in integration_data.items() if key in sys.argv[1:]}
+
+# creating folders for the logs
+for test_key, _ in integration_data.items():
+    if not os.path.exists(f"/home/{test_key}"):
+        os.mkdir(f"/home/{test_key}")
+
+# All keys available in a general default report
+# to check syntax of filters on the fly
+KEYS_AVAILABLE = all_keys_dicts(TREE_CHECKER)
+
+# data structures to count and cycle through targets
+targets_done = set()
+iter_tests = cycle(integration_data.items())
+total_targets = sum([len(test["targets"]) for _, test in integration_data.items()])
+
+# If any target recieve too many requests, it might not have
+# started well, this is another way to fill the set to break
+# the loop
+requests_counter = defaultdict(int)
+MAX_REQ = 25
+
+# Running wapiti for each module for each target
+# If a target isn't set up, passing to another and so on
+# That way we don't have a strict order and spare testing time
+for key_test, content_test in iter_tests:
+    if len(targets_done) == total_targets:
+        break
+    for target in content_test["targets"]:
+        if target['uid'] not in targets_done:
+            sys.stdout.write(f"Querying target {target['name']}...\n")
+            requests_counter[target['name']] += 1
+            try:
+                requests.get(f"{target['name']}", verify=False)
+                json_output_path = f"/home/{key_test}/{re.sub('/','_',re.sub(r'^https?://', '', target['name']))}.out"
+
+                # We define supplementary arguments globally and for each target:
+                more_args = target.get('supplementary_argument', '') + \
+                    ('' if target.get("erase_global_supplementary", False)
+                     else content_test.get('supplementary_argument', ''))
+
+                # We then call wapiti on each target of each module, generating a detailed JSON report
+                os.system(f"wapiti -u {target['name']} -m {content_test['modules']} "
+                          f"-f json -o {json_output_path} "
+                          f"{more_args} "
+                          f"--detailed-report --flush-session --verbose 2 ")
+                # Now we reparse the JSON to get only useful tests informations:
+                with open(json_output_path, "r") as bloated_output_file:
+                    bloated_output_data = json.load(bloated_output_file)
+                with open(json_output_path, "w") as output_file:
+
+                    # is a filter_tree supplied for this test ?
+                    if "report_filter_tree" in content_test and len(content_test["report_filter_tree"]):
+                        filter_tree = content_test["report_filter_tree"]
+                        # We look for key that CANNOT exist at all,
+                        # not even with a full report
+                        filter_keys = all_keys_dicts(filter_tree)
+                        assert filter_keys.issubset(KEYS_AVAILABLE), \
+                            f"Keys not existing at all: {filter_keys - KEYS_AVAILABLE}"
+                    else:
+                        filter_tree = DEFAULT_FILTER_TREE
+
+                    filtered_data = filter_data(bloated_output_data, filter_tree)
+
+                    # Some dates and other non determinist data
+                    # still exists somewhere in the detailed report
+                    bloated_output_data.get("infos", {}).pop("date", None)
+                    purge_irrelevant_data(filtered_data)
+
+                    # Arrays in JSONs needs to be ordered to prevent false positive
+                    sort_lists_in_dict(filtered_data)
+
+                    # Rewriting the file
+                    json.dump(filtered_data, output_file, indent=4)
+                targets_done.add(target['uid'])
+            except requests.exceptions.ConnectionError:
+                sys.stdout.write(f"Target {target['name']} from test {key_test} is not read...(yet ?)\n")
+                # 0.5 seconds penalty in case of no response to avoid requests spamming and being
+                # too fast at blacklisting targets
+                sleep(0.5)
+            if requests_counter[target['name']] > MAX_REQ:
+                sys.stdout.write(
+                    f"Target {target['name']} from test {key_test} takes too long to respond\nSkipping...\n")
+                targets_done.add(target['uid'])
