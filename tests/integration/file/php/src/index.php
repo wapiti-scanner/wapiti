f4c8f44b22f76904072e8003217b215d02f36321
RMI78 <romain.levifve@protonmail.com>
2023-05-10 12:38:13 +0000

    added test files for file module

--- tests/integration/file/php/src/index.php
+++ tests/integration/file/php/src/index.php
@@ -7,8 +7,9 @@
 	<h1>Index</h1>
 	<p>This is a simple PHP web page.</p>
     <ul>
-        <li><a href="url_injection.php">link 1</a></li>
-        <li><a href="file_injection.php">link 2</a></li>
-    </ul>
+        <li><a href="local_file_disclosure.php?file=inject">Local file disclosure</a></li>
+        <li><a href="code_execution.php?file=inject">code execution</a></li>
+        <li><a href="source_code_disclosure.php?file=inject">source code disclosure</a></li>
+        </ul>
 </body>
 </html>
