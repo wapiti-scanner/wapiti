2e81fb43a634e1044b08b32d8e5305153742f004
RMI78 <romain.levifve@protonmail.com>
2023-05-10 15:34:17 +0000

    Added tests files for the http_headers module

---
+++ tests/integration/test_mod_http_headers/php/src/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Index</title>
+</head>
+<body>
+	<h1>Index</h1>
+	<p>This is a simple PHP web page.</p>
+    <ul>
+        <li><a href="high_sec_http_header.php">link 3</a></li>
+        <li><a href="low_sec_http_header.php">link 1</a></li>
+        <li><a href="no_sec_http_header.php">link 2</a></li>
+
+    </ul>
+</body>
+</html>
---
+++ tests/integration/test_mod_http_headers/php/src/valid_sec_http_header.php
@@ -0,0 +1,14 @@
+<?php
+header('X-Frame-Options: DENY');
+header('X-Content-Type-Options: nosniff');
+header('Strict-Transport-Security: max-age=31536000; includeSubDomains; preload');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Headers tests</title>
+</head>
+<body>
+	<p>Lorem ipsum</p>
+</body>
+</html>
