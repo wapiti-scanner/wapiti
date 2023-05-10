97e3384a237f220a4dbcf3b179a42307454a9925
RMI78 <romain.levifve@protonmail.com>
2023-05-10 12:41:09 +0000

    Added test file for the htaccess module

---
+++ tests/integration/test_mod_htaccess/php/src/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <title>Htaccess test</title>
+    </head>
+    <body>
+        <p>Hey, that's a cool page you can GET</p>
+        <p>I bet you can't access <a href="./admin/private.php">this page.</a></p>        
+    </body>
+</html>
+
