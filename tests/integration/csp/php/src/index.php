6bccc20fe34bb69f12e0ee0830189e699ae85737
RMI78 <romain.escrime@gmail.com>
2023-05-16 15:30:32 +0000

    modified the index.php so it doesn't have csp

--- tests/integration/csp/php/src/index.php
+++ tests/integration/csp/php/src/index.php
@@ -1,5 +1,5 @@
 <?php
-header("Content-Security-Policy: default-src 'none'; script-src 'self'; connect-src 'self'; img-src 'self'; style-src 'self'; font-src 'self'; frame-src 'none'; object-src 'none'; media-src 'self'; manifest-src 'self'; base-uri 'self'; form-action 'self'; frame-ancestors 'none'; reflected-xss block; report-uri /csp-report-endpoint/");
+header_remove();
 ?>
 
 <!DOCTYPE html>
@@ -12,8 +12,7 @@ header("Content-Security-Policy: default-src 'none'; script-src 'self'; connect-
 	<p>This is a simple PHP web page.</p>
     <ul>
         <li><a href="csp_secured.php">link 1</a></li>
-        <li><a href="csp_missing_feature.php">link 2</a></li>
-        <li><a href="csp_insecured.php">link 3</a></li> 
+        <li><a href="csp_insecured.php">link 2</a></li> 
     </ul>
 </body>
 </html>
