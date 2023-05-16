efed477d5d4096364346610f0c89fb18a4b48268
RMI78 <romain.escrime@gmail.com>
2023-05-16 14:14:36 +0000

    Bash scripts reinforcements

--- tests/integration/run.sh
+++ tests/integration/run.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-docker compose -f docker-compose.setup.yml up --abort-on-container-exit 
+# exit upon any error
+set -o errexit
+
+# exit upon using undeclared variables
+set -o nounset
+
+#docker compose -f docker-compose.setup.yml up --abort-on-container-exit 
 
 # will do some checks only where assertions files exist
-asserters=$(find . -mindepth 2 -type d -name assertions)
+declare -a asserters=()
+mapfile -t asserters < <(find . -mindepth 2 -type d -name assertions)
 
-for path in ${asserters}; do
-    cd  "${path}"
+for path in "${asserters[@]}"; do
+    cd "${path}" || exit
     bash "check.sh"
-    cd - > /dev/null
+    cd - > /dev/null || exit
 done
