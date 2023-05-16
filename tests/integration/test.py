4f9a95dc7fc6dd5e3ae8e47567d58a09a3ec33e3
RMI78 <romain.escrime@gmail.com>
2023-05-16 09:41:09 +0000

    prevent infinite loop in test.py and typo in module.json

--- tests/integration/test.py
+++ tests/integration/test.py
@@ -4,6 +4,7 @@ import sys
 import requests
 import re
 from itertools import cycle
+from collections import defaultdict
 
 # parsing the json file containing the modules
 with open('/usr/local/bin/modules.json', 'r') as modules_file:
@@ -21,10 +22,17 @@ for mod in modules_data:
     if not os.path.exists(f"/home/{mod['module']}"):
         os.mkdir(f"/home/{mod['module']}")
 
+
 # data structures to count target and cycle through modules
 tested_targets = set()
 iter_modules = cycle(modules_data)
 total_targets = sum([len(mod["targets"]) for mod in modules_data])
+
+# If any target recieve too many requests, it might not have
+# started well, this is another way to fill the set to break
+# the loop
+requests_counter = defaultdict(int)
+MAX_REQ = 500
 
 # Running wapiti for each module for each target
 # If a target isn't set up, passing to another and so on
@@ -34,8 +42,8 @@ for mod in iter_modules:
         break
     for target in mod["targets"]:
         if target not in tested_targets:
-            sys.stdout.write(f"Querying target {target}...\n")
-            # print(f"Querying target {target}...")
+            sys.stdout.write(f"Querying target {target} from {mod['module']}...\n")
+            requests_counter[target] += 1
             try:
                 requests.get(f"http://{target}")
                 os.system(f"wapiti -u http://{target} -m {mod['module']} "
@@ -43,6 +51,9 @@ for mod in iter_modules:
                           f" --flush-session")
                 tested_targets.add(target)
             except requests.exceptions.ConnectionError:
-                sys.stdout.write(f"Target {target} is not ready yet...\n")
+                sys.stdout.write(f"Target {target} from module {mod['module']} is not ready yet...\n")
                 # print(f"Target {target} is not ready yet...")
                 pass
+        if requests_counter[target] > MAX_REQ:
+            sys.stdout.write(f"Target {target} from module {mod['module']} takes too long to respond\nSkipping...\n")
+            tested_targets.add(target)
