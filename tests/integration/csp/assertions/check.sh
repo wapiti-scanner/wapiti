7f420717bd10e702265bdcbe7c86d2be518b03f0
RMI78 <romain.escrime@gmail.com>
2023-05-19 10:20:55 +0000

    comments and code formating and expliciting

--- tests/integration/csp/assertions/check.sh
+++ tests/integration/csp/assertions/check.sh
@@ -13,10 +13,11 @@ die(){
 
 MODULE_NAME=$(pwd | cut -d'/' -f8)
 
-# get all the assertions (paths to text files that we want to find in the report)
-# 1 assertion per target, one or more targets per module
+# get all the assertions (paths to text files that we want to
+# find in the report).
+# 1 assertion per target, one or more targets per module,
 # assertions can be found in the same directory than this file
-declare -a assertions=()
+declare -a assertions
 mapfile -t assertions < <(find . -name "*.txt")
 if [[ ${#assertions[@]} -eq 0 ]]; then 
     die "Error: No assertions found in module $MODULE_NAME"
@@ -24,9 +25,8 @@ fi
 
 # Since the assertions and the target reports must have the same name 
 # (except their extention names, .txt and .out) we extract the targets names
-# from the assertion array
-# We also use their names to log some informations so keeping an array is
-# somewhat useful
+# from the assertion array. We also use their names to log some informations 
+# so keeping an array is somewhat useful
 declare -a targets_name
 for i in "${assertions[@]}"; do
     filename="$(basename "$i")"
@@ -35,20 +35,19 @@ done
 
 
 # We store the target reports paths in an array with respect to the target_name 
-# array order 
-# That way, our third array is ordered with the 2 other ones and we can safely 
-# use indexes to compare
+# array order. That way, our third array is ordered with the 2 other ones and 
+# we can safely use indexes to compare
 declare -a outputs
-for i in "${targets_name[@]}"; do
-    outputs+=("$(find ../../.test/"$MODULE_NAME"/ -name "$i.out")")
+for target in "${targets_name[@]}"; do
+    outputs+=("$(find ../../.test/"$MODULE_NAME"/ -name "$target.out")")
 done
 if [[ ${#outputs[@]} -eq 0 ]]; then 
     die "Error: No targets found in module $MODULE_NAME"
 fi
 
-# A special case is if we don't get the same number of reports as they are targets
+# A special case is if we don't get the same number of reports as they are targets.
 # Wapiti may not detect some targets or it can be due to various misconfigurations
-# So we check the size of each array to be sure they match
+# so we check the size of each array to be sure they match
 if [[ ${#assertions[@]} -ne ${#targets_name[@]} || ${#targets_name[@]} -ne ${#outputs[@]} ]] ; then
     die "Error: different number of reports/assertion files, found ${#outputs[@]} outputs for ${#assertions[@]} assertions"
 fi
@@ -56,7 +55,7 @@ fi
 # Comparing outputs and assertions is a 2-steps process:
 for i in "${!outputs[@]}"; do
     # First, we get rid of the beginning of the output by finding 
-    # The first common line between the assertion and the output
+    # the first common line between the assertion and the output
     mapfile -t assertion_content < "${assertions[$i]}"
 
     common_line_nbr=$(grep -n -m 1 "${assertion_content[0]}" "${outputs[$i]}" | cut -d ":" -f 1)
@@ -70,7 +69,7 @@ for i in "${!outputs[@]}"; do
         # The test pass
         echo "Assertion ${targets_name[$i]} of module $MODULE_NAME respected"
     else 
-        # Or we have to dig down through the differences 
+        # Or we have to dig down through the differences
         # Lines that are in the output but not in the assertions
         echo "In module $MODULE_NAME, assertion ${targets_name[$i]}:"
         for line in "${relevant_output[@]}"; do
