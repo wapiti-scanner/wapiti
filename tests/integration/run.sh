3e25e840bfae20b46d1089a8c2389cc1748f032b
RMI78 <romain.escrime@gmail.com>
2023-05-19 12:46:55 +0000

    removed || exit and uncommented the docker compose line

--- tests/integration/run.sh
+++ tests/integration/run.sh
@@ -6,14 +6,14 @@ set -o errexit
 # exit upon using undeclared variables
 set -o nounset
 
-#docker compose -f docker-compose.setup.yml up --abort-on-container-exit 
+docker compose -f docker-compose.setup.yml up --abort-on-container-exit 
 
 # will do some checks only where assertions files exist
 declare -a asserters=()
 mapfile -t asserters < <(find . -mindepth 2 -type d -name assertions)
 
 for path in "${asserters[@]}"; do
-    cd "${path}" || exit
+    cd "${path}" 
     bash "check.sh"
-    cd - > /dev/null || exit
+    cd - > /dev/null 
 done
