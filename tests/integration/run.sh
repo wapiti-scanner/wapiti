e642c97c05382ad8886413d37c5292459f8e60fd
RMI78 <romain.escrime@gmail.com>
2023-06-26 13:09:19 +0000

    renaming folder/variables to be more meaningful

--- tests/integration/run.sh
+++ tests/integration/run.sh
@@ -6,7 +6,13 @@ set -o errexit
 # exit upon using undeclared variables
 set -o nounset
 
-docker compose -f docker-compose.setup.yml up --abort-on-container-exit 
+# Cleaning docker
+docker container prune -f || echo "no containers to prune"
+docker volume prune -f || echo "no volumes to prune"
+docker rmi $(docker images -a -q) || echo "no images to remove"
+(docker system prune -f && docker network create test-network) || echo "no need to prune the system"
+
+time docker compose -f docker-compose.setup.yml up --abort-on-container-exit 
 
 # will do some checks only where assertions files exist
 declare -a asserters=()
