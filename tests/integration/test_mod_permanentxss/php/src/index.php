4b82fa64245d0ca2c74c98f00829c3e2d5ecfad6
RMI78 <romain.escrime@gmail.com>
2023-06-30 12:34:49 +0000

    added test files for permanentxss module

---
+++ tests/integration/test_mod_permanentxss/php/src/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome VulnWebsite</title>
+</head>
+<body>
+	<p>This is a simple page.</p>
+	<ul>
+    <li>this is a <a href="form.php?level=1">first link</a> to a simple xss injection</li>
+    <li>this is a <a href="form.php?level=2">second link</a> to a partially sanitized one</li>
+    <li>this is a <a href="form.php?level=3">third link</a> to the harshiest one</li>
+	</ul>
+</body>
+</html>
+
