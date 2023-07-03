db04732e146ba37a40e9cf145810614401c7d80f
RMI78 <romain.escrime@gmail.com>
2023-07-03 08:17:51 +0000

    added test files for the redirect module

---
+++ tests/integration/test_mod_redirect/php/src/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>Redirect</title>
+</head>
+<body>
+    <ul>
+        <li><a href="/redirect.php?link=None">redirect link</a></li>
+        <li><a href="/no_redirect.php?link=Whatever">no redirect link</a></li>
+    </ul>
+    
+</body>
+</html>
