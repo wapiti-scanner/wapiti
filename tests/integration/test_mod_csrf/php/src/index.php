53512e2c13b65a0fdd43b2c71f637d2b247737da
RMI78 <romain.escrime@gmail.com>
2023-07-25 10:46:51 +0000

    Added test files for the CSRF module

---
+++ tests/integration/test_mod_csrf/php/src/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Index</title>
+</head>
+<body>
+	<h1>Index</h1>
+	<p>This is a simple PHP web page.</p>
+    <ul>
+        <li><a href="unprotected_form.php">unprotected form</a></li>
+        <li><a href="protected_form.php">protected form</a></li>
+        <li><a href="fake_protected_form.php">weak protected form</a></li> 
+    </ul>
+</body>
+</html>
