9f93179715e1ee8c2363bc02d854d40d81ba648f
Ryukouss <benghechoua.oussama@gmail.com>
2024-03-14 19:55:57 +0100

    Add Citrix to mod_network_device
    Add Citrix products detection for module network_device

--- wapitiCore/attack/mod_network_device.py
+++ wapitiCore/attack/mod_network_device.py
@@ -2,6 +2,7 @@ from asyncio import Event
 from typing import Optional
 
 from wapitiCore.attack.network_devices.mod_harbor import ModuleHarbor
+from wapitiCore.attack.network_devices.mod_citrix import ModuleCitrix
 from wapitiCore.attack.network_devices.mod_forti import ModuleForti
 from wapitiCore.attack.network_devices.mod_ubika import ModuleUbika
 from wapitiCore.attack.attack import Attack
@@ -25,7 +26,7 @@ class ModuleNetworkDevice(Attack):
     async def attack(self, request: Request, response: Optional[Response] = None):
         self.finished = True
         request_to_root = Request(request.url)
-        modules_list = [ModuleHarbor, ModuleUbika, ModuleForti]
+        modules_list = [ModuleUbika, ModuleForti, ModuleHarbor, ModuleCitrix]
         for module in modules_list:
             mod = module(
                 self.crawler, self.persister, self.options, Event(), self.crawler_configuration
